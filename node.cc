36582eda13d1555be743c0d313e44b5886aedf9b
phouse512 <philiphouse2015@u.northwestern.edu>
2014-03-13 20:41:48 -0500

    test3

--- node.cc
+++ node.cc
@@ -176,12 +176,12 @@ void Node::LinkUpdate(const Link *l)
   cerr << *this << ": Linkupdate " << *l << endl;
 
   cerr << "Neighbor: " << neighbor << endl;
-  //if( (neighbor->cost > l->GetLatency() && neighbor!=NULL) //update if new latency is less
-    //  || (neighbor->dest_node == neighbor->next_node) //update if new latency is greater but prev was a direct link
-      //|| (neighbor == NULL) ){ //update if link is new
+  if( (neighbor->cost > l->GetLatency() && neighbor!=NULL) //update if new latency is less
+      || (neighbor->dest_node == neighbor->next_node) //update if new latency is greater but prev was a direct link
+      || (neighbor == NULL) ){ //update if link is new
       //WHAT ABOUT ZOMBIE ENTRIES OR DELETIONS?
   
-if(neighbor->next_node == l->GetDest() || neighbor->cost < 1 || neighbor->cost < l->GetLatency()){
+//if((neighbor->next_node == l->GetDest() || neighbor->cost < 1 || neighbor->cost < l->GetLatency()){
 
     const Row new_neighbor(l->GetDest(), l->GetDest(), l->GetLatency());
     table.SetNext(neighbor->dest_node, new_neighbor);
